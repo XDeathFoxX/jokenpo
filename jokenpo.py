e63392d5a11ecd4bec6c8073075931dd5df94537
XDeathFoxX <valdemirvoltatoni@gmail.com>
2022-03-28 21:18:20 -0300

    jokenpo

---
+++ jokenpo.py
@@ -0,0 +1,149 @@
+from random import choice as cho
+
+
+def jogar_novamente():
+    escolhas='sim','nao'
+    jogar_novamente=input('Deseja jogar novamente?(Sim/Não): ').lower().strip()
+    jogar_novamente=jogar_novamente.replace('ã','a')
+    if jogar_novamente=='sim':
+        jokenpo()
+    if jogar_novamente=='nao':
+        exit()
+    if jogar_novamente not in escolhas:
+        while jogar_novamente not in escolhas:
+            print('Digite Sim ou Não: ')
+            jogar_novamente=input('Deseja jogar novamente?(Sim/Não): ').lower().strip()
+        else:
+            if jogar_novamente=='sim':
+                jokenpo()
+            if jogar_novamente=='nao':
+                exit() 
+
+def jokenpo():
+    escolhas = 'pedra','papel','tesoura'
+    escolhas0 = 'Pedra, Papel, Tesoura   '
+    pedra = 'pedra'
+    papel = 'papel'
+    tesoura = 'tesoura'
+    pc = cho(escolhas) 
+    print()
+    jogada = input(f'Escolha a sua mão: {escolhas0}').strip().lower()
+    
+    if jogada in escolhas:
+        if jogada == pedra and pc == tesoura:
+            print()
+            print('Você venceu,Pedra vence Tesoura')
+            print()
+            jogar_novamente()  
+            
+        elif jogada == pedra and pc == papel:
+            print()
+            print('Você perdeu,Papel vence Pedra')
+            print()
+            jogar_novamente()
+
+        elif jogada == tesoura and pc == papel:
+            print()
+            print('Você venceu,Tesoura vence Papel')
+            print()
+            jogar_novamente()
+
+        elif jogada == tesoura and pc == pedra:
+            print()
+            print('Você perdeu,Pedra vence Tesoura')
+            print()
+            jogar_novamente()
+
+        elif jogada == papel and pc == pedra:
+            print()
+            print('Você venceu,Papel vence Pedra')
+            print()
+            jogar_novamente()
+
+        elif jogada == papel and pc == tesoura:
+            print()
+            print('Você perdeu,Tesoura vence Papel')
+            print()
+            jogar_novamente()
+
+        elif jogada and pc == tesoura:
+            print()
+            print('Empate,Tesoura não vence Tesoura    ')
+            print()
+            jogar_novamente()
+
+        elif jogada == papel and pc == papel:
+            print()
+            print('Empate,Papel não vence Papel   ')
+            print()
+            jogar_novamente()
+
+        elif jogada and pc == pedra:
+            print()
+            print('Empate,Pedra não vence Pedra  ')
+            print()
+            jogar_novamente()
+
+    else:
+        while jogada not in escolhas:
+            print()
+            print('Mão inexistente,digite Pedra ou Papel ou Tesoura  ')
+            print()
+            jogada = input(f'Escolha a sua mão: {escolhas0}').strip().lower()
+        else:
+            print(f'Você escolheu {jogada.title()} e o jogo escolheu {pc.title()}')
+            if jogada == pedra and pc == tesoura:
+                print()
+                print('Você venceu,Pedra vence Tesoura')
+                print()
+                jogar_novamente()  
+            
+            elif jogada == pedra and pc == papel:
+                print()
+                print('Você perdeu,Papel vence Pedra')
+                print()
+                jogar_novamente()
+
+            elif jogada == tesoura and pc == papel:
+                print()
+                print('Você venceu,Tesoura vence Papel')
+                print()
+                jogar_novamente()
+
+            elif jogada == tesoura and pc == pedra:
+                print()
+                print('Você perdeu,Pedra vence Tesoura')
+                print()
+                jogar_novamente()
+
+            elif jogada == papel and pc == pedra:
+                print()
+                print('Você venceu,Papel vence Pedra')
+                print()
+                jogar_novamente()
+
+            elif jogada == papel and pc == tesoura:
+                print()
+                print('Você perdeu,Tesoura vence Papel')
+                print()
+                jogar_novamente()
+
+            elif jogada and pc == tesoura:
+                print()
+                print('Empate,Tesoura não vence Tesoura    ')
+                print()
+                jogar_novamente()
+
+            elif jogada == papel and pc == papel:
+                print()
+                print('Empate,Papel não vence Papel   ')
+                print()
+                jogar_novamente()
+
+            elif jogada and pc == pedra:
+                print()
+                print('Empate,Pedra não vence Pedra  ')
+                print()
+                jogar_novamente()
+
+jokenpo()
